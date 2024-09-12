761ffeea09f3003b08f23000a71b330e538b8c94
Ana Vitoria <anavitoriadiasdasilva47@gmail.com>
2024-09-12 11:00:34 -0400

    add obejto POO

---
+++ 05-objeto.js
@@ -0,0 +1,16 @@
+class Pessoa {
+    constructor(nome, idade, altura, peso) { // Atributos
+        // Inicialização dos atributos
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+    // Método
+    falar() {
+        console.log(`Olá, meu nome é ${this.nome}!`);
+    }
+}
+// Objeto/Instância "pessoal" criada apartir da classe Pessoa
+const pessoa = new Pessoa('Ana Vitoria', 17, 1.60, 55.95);
+pessoal.falar(); // Chamada do método falar() da classe Pessoa
