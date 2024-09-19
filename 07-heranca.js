361b2a31dadf1cf2f05487fdff35141dff6cf6bf
Ana Vitoria <anavitoriadiasdasilva47@gmail.com>
2024-09-19 08:25:02 -0400

    add objeto

--- 07-heranca.js
+++ 07-heranca.js
@@ -37,3 +37,13 @@ class Professor extends Funcionario {
         super(nome, idade, altura, peso, salario);
     }
 }
+
+// Criando objetos aluno, diretor e professor
+const aluno = new Estudante("João", 16, 1.70, 70, "Programação de Jogos Digitais");
+const diretor = new Diretor("Sérgio", 45, 1.65, 65, 3000);
+const professor = new Professor("Wanderson", 40, 1.65, 70, 1000);
+
+// Mostrando no console a saída 
+console.log(aluno); //Saída: Estudante {nome: 'João', idade: 16, altura: 1.7, peso: 70, curso: 'Programação de Jogos Digitais'}
+console.log(diretor); //Saída: Diretor {nome: 'Sérgio', idade: 45, altura: 1.65, peso: 65, salario: 3000}
+console.log(professor); //Saída: Professor {nome: 'Wanderson', idade: 40, altura: 1.65, peso: 70, salario: 1000}
