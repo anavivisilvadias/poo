854881b82c98c394bfc74eacf4d45a78388bf5f7
Ana Vitoria <anavitoriadiasdasilva47@gmail.com>
2024-10-01 08:31:54 -0400

    add polimorfismo encapsulamento

--- 16-heranca-polimorfismo-encapsulamento.js
+++ 16-heranca-polimorfismo-encapsulamento.js
@@ -51,4 +51,27 @@ class Personagem {
   set mana(mana) {
     this.#mana = mana;
   }
-}
+ // Métodos
+ // Simulando sobrecarga com rest parameter
+ atacar(...args) {
+        if (args.length === 0) {
+            console.log(`${this.nome} realiza um ataque normal!`);
+        } else if (args.length === 1) {
+            console.log(`${this.nome} atacou com um poder de ${args[0]}!`);
+        } else if (args.length === 2) {
+                console.log(`${this.nome} dispara flechas ${tipoFlecha} com um arco!`);
+            } else {
+                console.log(`${this.nome} usou ${args[1]} e atacou com ${args[0]} de poder!`);
+        } else {
+            console.log('Número inválido de argumentos.');
+        }
+    }
+    defesa() {
+        console.log(`${this.nome} se defendeu com ${this.#nivel * 2} pontos de defesa!`);
+    }
+    receberDano(dano) {
+        this.vida -= dano;
+        console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
+        }
+    }
+
