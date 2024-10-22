aac73bdd3e088dc1b291b0b4ede623bdeb73fec6
Ana Vitoria <anavitoriadiasdsilva47@gmail.com>
2024-10-22 10:07:47 -0400

    add classe mago e guerreiro

--- 18-classe-abstrata.java
+++ 18-classe-abstrata.java
@@ -11,3 +11,49 @@ abstract class Personagem {
     public abstract void atacar();
     public abstract void defender();
 }
+
+// Classe derivada "Guerreiro"
+class Guerreiro extends Personagem {
+    private String arma;
+
+    // Constructor da classe Guerreiro
+    public Guerreiro(String nome, String arma) {
+        super(nome);
+        this.arma = arma;
+    }
+
+    // Sobrescrita do método Atacar da classe Abstrata Personagem
+    @Override
+    public void atacar() {
+        System.out.println(nome + " ataca com a " + arma + "!");
+    }
+
+    // Sobrescrita do método defender da classe Abstrata Personagem 
+    @Override
+    public void defender() {
+        System.out.println(nome + " levanta o escudo para se defender!");
+    }
+}
+
+// Classe derivada "Mago"
+class Mago extends Personagem {
+    private String magia;
+
+    // Constructor da classe Mago
+    public Mago(String nome, String magia) {
+        super(nome);
+        this.magia = magia;
+    }
+
+    // Sobrescrita do método Atacar da classe Abstrata Personagem
+    @Override
+    public void atacar() {
+        System.out.println(nome + " lança a magia " + magia + "!");
+    }
+
+    // Sobrescrita do método defender da classe Abstrata Personagem 
+    @Override
+    public void defender() {
+        System.out.println(nome + " usa um escudo mágico para se defender! ");
+    }
+}
